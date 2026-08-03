661e20d5f038ba77090292fc91c06f762e15ab10
Vansh_Idnani <vanshidnani6@gmail.com>
2026-08-03 10:36:16 +0530

    Time: 0 ms (100%), Space: 9.6 MB (33.33%) - LeetHub

---
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int zeros = 0, ones = 0, result = 0;
+        for (char c : s) {
+            if (c == '0') zeros++;
+            else ones++;
+            if (abs(zeros - ones) <= 1) {
+                result++;
+            }
+        }
+        return result;
+    }
+};
